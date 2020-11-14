79a6d3367cd90a277fd7324719559a72983a6e20
gabriel <aballaygabriel@gmail.com>
2020-11-14 20:00:11 -0300

    rest server - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,23 @@
 //====================
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+//====================
+//  Entorno
+//====================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'env';
+
+
+//====================
+//  Base de Datos
+//====================
+
+let urlDB;
+if (process.env.NODE_ENV === 'env') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://cafe1234:cafe1234@cluster0.nttvu.mongodb.net/cafe?retryWrites=true&w=majority';
+}
+process.env.URLDB = urlDB;
