deba30e124cd4439fa97e4a1ed22c5ad1cb4bae9
gabriel <aballaygabriel@gmail.com>
2020-11-16 00:29:36 -0300

    url DDBB protegida

--- server/config/config.js
+++ server/config/config.js
@@ -20,6 +20,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'env') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://cafe1234:cafe1234@cluster0.nttvu.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI; //+'&w=majority';
 }
 process.env.URLDB = urlDB;
